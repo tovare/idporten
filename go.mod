0767d3ba5cf1b06872fcabadb120ab89ccf67426
Tov Are Jacobsen <mail@tovare.com>
2020-11-23 23:36:52 +0100

    Progress on SendEverythingToBigquery

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/tovare/idporten
 
 go 1.13
 
+require cloud.google.com/go/bigquery v1.13.0 
--- idharvest.go
+++ idharvest.go
@@ -22,11 +22,14 @@
 package idharvest
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
+
+	"cloud.google.com/go/bigquery"
 )
 
 // Converts  a timestamp to the format 2014-05-01T20:00:00Z.
@@ -72,4 +75,81 @@ func Query(from time.Time, to time.Time, orgnum Org) (stat []Statistikk, err err
 	}
 	json.Unmarshal(body, &stat)
 	return
+}
+
+var datasetName string = "idporten"
+var tableName string = "nav"
+var projectID = "homepage-961"
+
+// SendEverythingToBigquery proocesses all historical data and sends it to BigQuery.
+// This process may take a few minutes and shuold be called locally. This procedure
+// is destructive to all historical data.
+//
+// Preparing BigQuery
+//
+// A new dataset is created if it doens´t exist. A shema is inferred from the
+// Statistikk struct and a a table is created for the data. If a table exist
+// all data is deleted.
+//
+// Processing data
+//
+// All data is read from both organization numbers and merged using a map
+// structure. Once complete all entries are extracted and and the array
+// is sorted befre streaming the content to BigQuery.
+//
+func SendEverythingToBigquery() (err error) {
+
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return
+	}
+	defer client.Close()
+
+	// Create a dataset if it doesn´t exist.
+	if _, err := client.Dataset(datasetName).Metadata(ctx); err != nil {
+		meta := &bigquery.DatasetMetadata{
+			Description: "Statistikk om innlogginger fra idporten",
+			Location:    "EU", // See https://cloud.google.com/bigquery/docs/locations
+		}
+		if err := client.Dataset("roundtrip").Create(ctx, meta); err != nil {
+			return err
+		}
+	}
+	schema, err := bigquery.InferSchema(Statistikk{})
+	if err != nil {
+		return
+	}
+	metaData := &bigquery.TableMetadata{
+		Schema:         schema,
+		ExpirationTime: time.Now().AddDate(2, 0, 0), // Table will be automatically deleted in 2 years.
+	}
+	tableRef := client.Dataset(datasetName).Table(tableName)
+
+	// Delete the table if it exists.
+	_, err = tableRef.Metadata(ctx)
+	if err == nil {
+		if err := tableRef.Delete(ctx); err != nil {
+			return err
+		}
+	}
+
+	if err := tableRef.Create(ctx, metaData); err != nil {
+		return err
+	}
+
+	// Process data from idporten.
+	/*
+		items2 := []TestSchema{
+			{MyTime: time.Now().AddDate(0, 0, 0), MyValue: 10},
+			{MyTime: time.Now().AddDate(0, 0, 1), MyValue: 11},
+			{MyTime: time.Now().AddDate(0, 0, 2), MyValue: 12},
+			{MyTime: time.Now().AddDate(0, 0, 3), MyValue: 13},
+		}
+
+		if err := tableRef.Inserter().Put(ctx, items2); err != nil {
+			fmt.Println("Failed to insert values", err)
+		}
+	*/
+	return err
 }
